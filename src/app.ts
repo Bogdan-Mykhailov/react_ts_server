fb61e99e3305e978c71def310f371e28ae7ee983
Bogdan <bogdan_mykhailov@icloud.com>
2023-08-04 11:03:41 +0300

    :construction: add socket.io implement session counter

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,43 @@
 'use strict';
 
 import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(
+  server,
+  {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST'],
+    },
+  },
+);
 
 const PORT = 4000;
-const createServer = () => {
-  const app = express();
 
-  app.use((req, res) => {
-    res.send('Hello World');
+let activeSessions = 0;
+
+io.on('connection', (socket) => {
+  activeSessions += 1;
+  io.emit('sessionCountUpdate', activeSessions);
+
+  socket.on('disconnect', () => {
+    activeSessions -= 1;
+    io.emit('sessionCountUpdate', activeSessions);
   });
 
-  return app;
-};
+  socket.on('getSessionCount', () => {
+    io.to(socket.id).emit('sessionCountUpdate', activeSessions);
+  });
+});
 
-createServer().listen(PORT, () => {
+app.use((req, res) => {
+  res.send('Hello World');
+});
+
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
