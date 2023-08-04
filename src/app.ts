993b36788680df928d91b65cf8afdab47cf1ab61
Bogdan <bogdan_mykhailov@icloud.com>
2023-08-04 18:27:27 +0300

    :construction: remove unused cod

--- src/app.ts
+++ src/app.ts
@@ -34,10 +34,6 @@ io.on('connection', (socket) => {
   });
 });
 
-app.use((req, res) => {
-  res.send('Hello World!');
-});
-
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
